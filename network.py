c19169eefc035e26abf2b01212f4782b133f4971
Đặng Đức Tùng <dangductungcfc@gmail.com>
2017-04-07 01:13:50 +0700

    Update network.py

--- network.py
+++ network.py
@@ -12,15 +12,11 @@ print X_train.shape
 
 #Calculate pixels of images
 num_pixels = X_train.shape[1] * X_train.shape[2] # 28x28
-
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
-
 #Take input to [0,1]
 X_train = X_train / 255
 X_test = X_test / 255
-
-#one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
